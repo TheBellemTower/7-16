f685e82660fa683af4c57fc274a4418f68ea241a
Ethan Bellem <ethan.bellem@ocsbstudent.ca>
2018-05-29 14:42:00 -0400

    stawey

--- scenes/gameScene.lua
+++ scenes/gameScene.lua
@@ -16,21 +16,23 @@ local scene = composer.newScene()
 local ninjaBoy = nil
 local map = nil
 local rightArrow = nil
+local jumpButton = nil
+local shootButton = nil
+local playerBullets = {}
 
 -- Function to chaneg sequence 
-
 local function onRightArrowClicked( event )
     if ( event.phase == "began" ) then 
         if ninjaBoy.sequence ~= "run" then 
             ninjaBoy.sequence = "run"
-            ninjaBoy:setSequence( "runBoy" )
+            ninjaBoy:setSequence( "run" )
             ninjaBoy:play()
         end    
     
     elseif ( event.phase == "ended" ) then   
         if ninjaBoy.sequence ~= "idle" then 
             ninjaBoy.sequence = "idle"
-            ninjaBoy:setSequence( "idleBoy" )
+            ninjaBoy:setSequence( "idle" )
             ninjaBoy:play()
         end 
     end 
@@ -38,9 +40,22 @@ local function onRightArrowClicked( event )
     return true    
 end
 
+local function onJumpButtonClicked( event )
+    if ( event.phase == "began" ) then 
+        if ninjaBoy.sequence ~= "jump" then
+            ninjaBoy.sequence = "jump"
+            ninjaBoy:setLinearVelocity( 150, -500 )
+            ninjaBoy:setSequence( "jump" )
+            ninjaBoy:play()
+        end
+   end  
+
+   return true  
+end
+
 -- move ninja
-local function moveNinjaRight( event )
-    if ninjaBoy.sequence == "runBoy" then
+local function moveNinja( event )
+    if ninjaBoy.sequence == "run" then
         transition.moveBy( ninjaBoy, {
             x = 10,
             y = 0,
@@ -48,7 +63,74 @@ local function moveNinjaRight( event )
             } )
     end     
     
+    if ninjaBoy.sequence == "jump" then
+        local linearVelocityX, linearVelocityY = ninjaBoy:getLinearVelocity()
+
+        if linearVelocityX <= 20 then
+            ninjaBoy.sequence = "idle"
+            ninjaBoy:setSequence( "idle" )
+            ninjaBoy:play()
+        end
+    end
+
     return true
+end
+
+
+local function resetAfterThrow( event )
+    ninjaBoy.sequence = "idle"
+    ninjaBoy:setSequence( "idle" )
+    ninjaBoy:play()
+end
+
+function onthrowButtonClicked( event )
+    if ( event.phase == "began" ) then
+        if ninjaBoy.sequence ~= "throw" then 
+            ninjaBoy.sequence = "throw"
+            ninjaBoy:setSequence( "throw" )
+            ninjaBoy:play()
+            timer.performWithDelay( 800, resetAfterThrow )
+
+        -- use function to delay throw to match animation
+            local function delayThrow( event )
+                local aSingleBullet = display.newImage( "./assets/sprites/Kunai.png" )
+                 -- puts bullet on screen at character postion
+                aSingleBullet.x = ninjaBoy.x
+                aSingleBullet.y = ninjaBoy.y
+                physics.addBody( aSingleBullet, 'dynamic' )
+                -- Makes sprite a "bullet" type object
+                aSingleBullet.isBullet = true
+                aSingleBullet.gravityScale = 0
+                aSingleBullet.id = "bullet"
+                aSingleBullet:setLinearVelocity( 1500, 0 )
+                aSingleBullet.isFixedRotation = true
+        
+                table.insert(playerBullets,aSingleBullet)
+                print("# of bullet: " .. tostring(#playerBullets))    
+            end 
+        timer.performWithDelay( 200, delayThrow )
+        
+        
+        end
+    end
+
+    return true
+end
+
+local function checkPlayerBulletsOutOfBounds()
+    -- check if bullets are off the screen and rmoves them 
+    local bulletCounter
+
+    if #playerBullets > 0 then
+        for bulletCounter = #playerBullets, 1 ,-1 do
+            if playerBullets[bulletCounter].x > display.contentWidth + 1000 then
+                playerBullets[bulletCounter]:removeSelf()
+                playerBullets[bulletCounter] = nil
+                table.remove(playerBullets, bulletCounter)
+                print("remove bullet")
+            end
+        end
+    end
 end
 
 -- create()
@@ -78,7 +160,7 @@ function scene:create( event )
     
     local sequence_data = {
         {
-            name = "idleBoy",
+            name = "idle",
             start = 1, 
             count = 10,
             time = 800, 
@@ -86,7 +168,7 @@ function scene:create( event )
             sheet = sheetBoyIdle
         },
         {
-            name = "runBoy",
+            name = "run",
             start = 1, 
             count = 10,
             time = 800, 
@@ -94,19 +176,19 @@ function scene:create( event )
             sheet = sheetBoyRun
         },
         {
-            name = "jumpBoy",
+            name = "jump",
             start = 1, 
             count = 10,
-            time = 800, 
-            loopCount = 0,
+            time = 500, 
+            loopCount = 1,
             sheet = sheetBoyJump
         },
         {
-            name = "throwBoy",
+            name = "throw",
             start = 1, 
             count = 10,
-            time = 800, 
-            loopCount = 0,
+            time = 750, 
+            loopCount = 1,
             sheet = sheetBoyThrow
         }
     }
@@ -114,15 +196,15 @@ function scene:create( event )
 
     -- show ninjaBoy
     ninjaBoy = display.newSprite( sheetBoyIdle, sequence_data )
-    ninjaBoy.x = 200
+    ninjaBoy.x = 100
     ninjaBoy.y = 0
     ninjaBoy.sequence = "idle"
-    ninjaBoy.isFixedRotation = true
     ninjaBoy.id = "ninja Boy"
     physics.addBody( ninjaBoy, "dynamic", { 
         friction = 0.5, 
         bounce = 0.3 
         } )
+    ninjaBoy.isFixedRotation = true
     ninjaBoy:setSequence( "idleBoy" )
     ninjaBoy:play()
 
@@ -132,9 +214,23 @@ function scene:create( event )
     rightArrow.id = "right Arrow"
     rightArrow.alpha = 0.7
 
+    jumpButton = display.newImage( "./assets/sprites/jumpButton.png" )
+    jumpButton.x  = 1500
+    jumpButton.y = 1300
+    jumpButton.id = "jump Button"
+    jumpButton.alpha = 0.7
+
+    throwButton = display.newImage( "./assets/sprites/jumpButton.png" )
+    throwButton.x  = 1700
+    throwButton.y = 1300
+    throwButton.id = "throw Button"
+    throwButton.alpha = 0.7
+
     sceneGroup:insert( map )
     sceneGroup:insert( ninjaBoy )
     sceneGroup:insert( rightArrow )
+    sceneGroup:insert( jumpButton )
+    sceneGroup:insert( throwButton )
 
 end
 
@@ -148,8 +244,10 @@ function scene:show( event )
      
     elseif ( phase == "did" ) then
         rightArrow:addEventListener( "touch", onRightArrowClicked )
-        Runtime:addEventListener( "enterFrame", moveNinjaRight )
- 
+        jumpButton:addEventListener( "touch", onJumpButtonClicked )
+        throwButton:addEventListener( "touch", onthrowButtonClicked )
+        Runtime:addEventListener( "enterFrame", moveNinja )
+        Runtime:addEventListener( "enterFrame", checkPlayerBulletsOutOfBounds )
     end
 end
  
@@ -166,7 +264,10 @@ function scene:hide( event )
     elseif ( phase == "did" ) then
         -- Code here runs immediately after the scene goes entirely off screen
         rightArrow:removeEventListener( "touch", onRightArrowClicked )
-        Runtime:removeEventListener( "enterFrame", moveNinjaRight )
+        jumpButton:removeEventListener( "touch", onJumpButtonClicked )
+        throwButton:removeEventListener( "touch", onthrowButtonClicked )
+        Runtime:removeEventListener( "enterFrame", moveNinja )
+        Runtime:removeEventListener( "enterFrame", checkPlayerBulletsOutOfBounds )
     end
 end
  
